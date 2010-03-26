cc3497fb632ad1267850e9b0cad56fbdf18b3808
petrih3@gmail.com <petrih3@gmail.com@a08db57a-c636-5b31-1058-75176726527b>
2010-03-26 13:48:26 +0000

    Cleaned up mutator test code and vec library functions (dot3() and dot4() -> dot(), etc).

--- trunk/src/lveclib.c
+++ trunk/src/lveclib.c
@@ -15,22 +15,15 @@
 #include "lualib.h"
 
 static int vec_new (lua_State *L) {
-  float x = lua_tonumber(L, 1);
-  float y = lua_tonumber(L, 2);
-  float z = lua_tonumber(L, 3);
-  float w = lua_tonumber(L, 4);
+  float x = (float)lua_tonumber(L, 1);
+  float y = (float)lua_tonumber(L, 2);
+  float z = (float)lua_tonumber(L, 3);
+  float w = (float)lua_tonumber(L, 4);
   lua_pushvec(L, x, y, z, w);
   return 1;
 }
 
-static int vec_dot3 (lua_State *L) {
-  const float* v1 = luaL_checkvec(L, 1);
-  const float* v2 = luaL_checkvec(L, 2);
-  lua_pushnumber(L, v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]);
-  return 1;
-}
-
-static int vec_dot4 (lua_State *L) {
+static int vec_dot (lua_State *L) {
   const float* v1 = luaL_checkvec(L, 1);
   const float* v2 = luaL_checkvec(L, 2);
   lua_pushnumber(L, v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] + v1[3]*v2[3]);
@@ -44,26 +37,13 @@ static int vec_cross (lua_State *L) {
   return 1;
 }
 
-static int vec_length3 (lua_State *L) {
-  const float* v = luaL_checkvec(L, 1);
-  lua_pushnumber(L, sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]));
-  return 1;
-}
-
-static int vec_length4 (lua_State *L) {
+static int vec_length (lua_State *L) {
   const float* v = luaL_checkvec(L, 1);
   lua_pushnumber(L, sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2] + v[3]*v[3]));
   return 1;
 }
 
-static int vec_normalize3 (lua_State *L) {
-  const float* v = luaL_checkvec(L, 1);
-  float s = 1.0f / sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
-  lua_pushvec(L, v[0]*s, v[1]*s, v[2]*s, 0.0f);
-  return 1;
-}
-
-static int vec_normalize4 (lua_State *L) {
+static int vec_normalize (lua_State *L) {
   const float* v = luaL_checkvec(L, 1);
   float s = 1.0f / sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2] + v[3]*v[3]);
   lua_pushvec(L, v[0]*s, v[1]*s, v[2]*s, v[3]*s);
@@ -73,13 +53,10 @@ static int vec_normalize4 (lua_State *L) {
 
 static const luaL_Reg veclib[] = {
   {"new",        vec_new},
-  {"dot3",       vec_dot3},
-  {"dot4",       vec_dot4},
+  {"dot",        vec_dot},
   {"cross",      vec_cross},
-  {"length3",    vec_length3},
-  {"length4",    vec_length4},
-  {"normalize3", vec_normalize3},
-  {"normalize4", vec_normalize4},
+  {"length",     vec_length},
+  {"normalize",  vec_normalize},
   {NULL, NULL}
 };
 
